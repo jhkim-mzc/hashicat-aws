221d66085ce7100de05e7f5a8d0b8f87bf6e0390
Jonghyun Kim <67783656+jhkim-mzc@users.noreply.github.com>
2022-01-21 11:07:01 +0900

    Handson 3번째용 코드

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,4 @@
 # Outputs file
-output "catapp_url" {
-  value = "http://${aws_eip.hashicat.public_dns}"
-}
-
-output "catapp_ip" {
-  value = "http://${aws_eip.hashicat.public_ip}"
-}
+#output "catapp_url" {
+#  value = "http://${aws_instance.hashicat.public_dns}"
+#}
